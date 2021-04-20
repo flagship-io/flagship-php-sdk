b6963930633ee0601b05c61eea30b163ad36e8de
Merveille KITOKO <merkitoko@gmail.com>
2021-04-20 11:40:03 +0100

    Add FlagshipConfigTest.php with 72.73 code coverage

--- src/FlagshipConfig.php
+++ src/FlagshipConfig.php
@@ -2,11 +2,12 @@
 
 namespace Abtasty\FlagshipPhpSdk;
 
+use Abtasty\FlagshipPhpSdk\Enum\DecisionMode;
+use Abtasty\FlagshipPhpSdk\Enum\FlagShipConstant;
 use Abtasty\FlagshipPhpSdk\Interfaces\LogManagerInterface;
 
 /**
- * Class FlagshipConfig
- *
+ * Flagship SDK configuration class to provide at initialization.
  * @package Abtasty\FlagshipPhpSdk
  */
 class FlagshipConfig
@@ -22,27 +23,63 @@ class FlagshipConfig
     /**
      * @var int
      */
-    private $decisionMode;
+    private $decisionMode = DecisionMode::DECISION_API;
     /**
      * @var int
      */
-    private $timeOut = 2000;
+    private $timeOut = FlagShipConstant::REQUEST_TIME_OUT;
     /**
      * @var LogManagerInterface
      */
     private $logManager;
 
     /**
-     * FlagshipConfig constructor.
-     * @param $envId
-     * @param $apiKey
-     * @param $decisionMode
+     * Create a new FlagshipConfig configuration.
+     * @param string $envId : Environment id provided by Flagship.
+     * @param string $apiKey : Secure api key provided by Flagship.
      */
-    public function __construct($envId, $apiKey, $decisionMode)
+    public function __construct($envId, $apiKey)
     {
         $this->envId = $envId;
         $this->apiKey = $apiKey;
-        $this->decisionMode = $decisionMode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEnvId()
+    {
+        return $this->envId;
+    }
+
+    /**
+     * Specify the environment id provided by Flagship, to use.
+     * @param string $envId environment id.
+     * @return FlagshipConfig
+     */
+    public function setEnvId($envId)
+    {
+        $this->envId = $envId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * Specify the secure api key provided by Flagship, to use.
+     * @param string $apiKey secure api key.
+     * @return FlagshipConfig
+     */
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = $apiKey;
+        return $this;
     }
 
     /**
@@ -54,12 +91,15 @@ class FlagshipConfig
     }
 
     /**
-     * @param int $decisionMode
+     * Specify the SDK running mode.
+     * @param int $decisionMode decision mode value e.g DecisionMode::DECISION_API
      * @return FlagshipConfig
      */
-    public function setDecisionMode($decisionMode)
+    private function setDecisionMode($decisionMode)
     {
-        $this->decisionMode = $decisionMode;
+        if (DecisionMode::isDecisionMode($decisionMode)) {
+            $this->decisionMode = $decisionMode;
+        }
         return $this;
     }
 
@@ -72,12 +112,15 @@ class FlagshipConfig
     }
 
     /**
-     * @param int $timeOut
+     * Specify timeout for api request.
+     * @param int $timeOut milliseconds for connect and read timeouts. Default is 2000.
      * @return FlagshipConfig
      */
     public function setTimeOut($timeOut)
     {
-        $this->timeOut = $timeOut;
+        if (is_numeric($timeOut) && $timeOut > 0) {
+            $this->timeOut = $timeOut;
+        }
         return $this;
     }
 
@@ -90,7 +133,8 @@ class FlagshipConfig
     }
 
     /**
-     * @param LogManagerInterface $logManager
+     * Specify a custom implementation of LogManager in order to receive logs from the SDK.
+     * @param LogManagerInterface $logManager custom implementation of LogManager.
      * @return FlagshipConfig
      */
     public function setLogManager($logManager)
@@ -98,5 +142,4 @@ class FlagshipConfig
         $this->logManager = $logManager;
         return $this;
     }
-
 }
---
+++ tests/FlagshipConfigTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Abtasty\FlagshipPhpSdk;
+
+use Abtasty\FlagshipPhpSdk\Enum\DecisionMode;
+use Abtasty\FlagshipPhpSdk\Enum\FlagShipConstant;
+use PHPUnit\Framework\TestCase;
+
+class FlagshipConfigTest extends TestCase
+{
+
+    function configData()
+    {
+        return [[['envId' => 'env_value','apiKey' => 'key_value']]];
+    }
+
+    /**
+     * @dataProvider configData
+     * @param array $configData
+     */
+    public function testSetTimeOut($configData)
+    {
+        $timeOut = 5000;
+        $config = new FlagshipConfig($configData['envId'], $configData['apiKey']);
+
+        $this->assertEquals(FlagShipConstant::REQUEST_TIME_OUT, $config->getTimeOut());
+
+        $config->setTimeOut($timeOut);
+        $this->assertEquals($timeOut, $config->getTimeOut());
+
+        $config->setTimeOut(0);
+        $this->assertEquals($timeOut, $config->getTimeOut());
+
+        $config->setTimeOut("not a number");
+        $this->assertEquals($timeOut, $config->getTimeOut());
+    }
+
+    public function testSetLogManager()
+    {
+    }
+
+    /**
+     * @dataProvider configData
+     * @param array $configData
+     */
+    public function testSetApiKey($configData)
+    {
+        $newApiKey = 'new_api_key';
+        $config = new FlagshipConfig($configData['envId'], $configData['apiKey']);
+        $config->setApiKey($newApiKey);
+        $this->assertEquals($newApiKey, $config->getApiKey());
+    }
+
+    /**
+     * @dataProvider configData
+     * @param array $configData
+     */
+    public function testSetEnvId($configData)
+    {
+        $newEnvId = 'new_env_id';
+        $config = new FlagshipConfig($configData['envId'], $configData['apiKey']);
+        $config->setEnvId($newEnvId);
+        $this->assertEquals($newEnvId, $config->getEnvId());
+    }
+
+    /**
+     * @dataProvider configData
+     * @param array $configData
+     */
+    public function testDecisionMode($configData)
+    {
+        $config = new FlagshipConfig($configData['envId'], $configData['apiKey']);
+        $this->assertEquals(DecisionMode::DECISION_API, $config->getDecisionMode());
+    }
+
+    /**
+     * @dataProvider configData
+     * @param array $configData
+     */
+    public function test__construct($configData)
+    {
+        $config = new FlagshipConfig($configData['envId'], $configData['apiKey']);
+        $this->assertInstanceOf("Abtasty\FlagshipPhpSdk\FlagshipConfig", $config);
+        $this->assertEquals($config->getEnvId(), $configData['envId']);
+        $this->assertEquals($config->getApiKey(), $configData['apiKey']);
+    }
+}

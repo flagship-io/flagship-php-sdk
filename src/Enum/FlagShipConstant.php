f24c50690af4fca92c6d33d1bdf94a42fc783fde
Merveille KITOKO <merkitoko@gmail.com>
2021-04-20 17:20:19 +0100

    create Global SDK constants class

---
+++ src/Enum/FlagShipConstant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Abtasty\FlagshipPhpSdk\Enum;
+
+/**
+ * Global SDK constants
+ * @package Abtasty\FlagshipPhpSdk\Enum
+ */
+class FlagShipConstant
+{
+
+    /**
+     * Default request timeout
+     */
+    const REQUEST_TIME_OUT = 2000;
+}

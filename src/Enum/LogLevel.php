9ed7716b9b9948633a738dbb9c933785fa620dcc
Merveille KITOKO <merkitoko@gmail.com>
2021-04-21 18:33:20 +0100

    Refactor LogManagerInterface, add LogManager class and LogLevel Enum

---
+++ src/Enum/LogLevel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Flagship\Enum;
+
+class LogLevel
+{
+    const EMERGENCY = 'emergency';
+    const ALERT     = 'alert';
+    const CRITICAL  = 'critical';
+    const ERROR     = 'error';
+    const WARNING   = 'warning';
+    const NOTICE    = 'notice';
+    const INFO      = 'info';
+    const DEBUG     = 'debug';
+}
---
+++ src/utils/LogManager.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Flagship\utils;
+
+use Flagship\Interfaces\LogManagerInterface;
+
+class LogManager implements LogManagerInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function emergency($message, array $context = array())
+    {
+        // TODO: Implement emergency() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function alert($message, $context = null)
+    {
+        // TODO: Implement alert() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function critical($message, array $context = array())
+    {
+        // TODO: Implement critical() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function error($message, $context = null)
+    {
+        // TODO: Implement error() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function warning($message, $context = null)
+    {
+        // TODO: Implement warning() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function notice($message, $context = [])
+    {
+        // TODO: Implement notice() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function info($message, $context = [])
+    {
+        // TODO: Implement info() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function debug($message, $context = [])
+    {
+        // TODO: Implement debug() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function log($level, $message, array $context = array())
+    {
+        // TODO: Implement log() method.
+    }
+}

ac05d10dd658a6317be60d3a0ec6f02263f14c12
Merveille KITOKO <merkitoko@gmail.com>
2021-04-21 18:31:08 +0100

    Add new globals constants to FlagshipConstant.php

--- src/Enum/FlagshipConstant.php
+++ src/Enum/FlagshipConstant.php
@@ -13,4 +13,22 @@ class FlagshipConstant
      * Default request timeout
      */
     const REQUEST_TIME_OUT = 2000;
+
+    /**
+     * SDK language
+     */
+    const SDK_LANGUAGE = "PHP";
+
+    /**
+     * Decision api base url
+     */
+    const BASE_API_URL = "https://decision.flagship.io/v2";
+
+
+    const EXPOSE_ALL_KEYS = "exposeAllKeys";
+
+    /**
+     * SDK version
+     */
+    const SDK_VERSION = "v1";
 }

baf61e98d73f63b27486417d8f99391e51210357
Merveille KITOKO <merkitoko@gmail.com>
2021-04-22 17:56:27 +0100

    Move Validator method from Visitor to Validator

---
+++ src/Utils/Validator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Flagship\Utils;
+
+class Validator
+{
+
+    /**
+     * Return true if key is not empty and is a string, otherwise return false
+     * @param mixed $key Context key
+     * @return bool
+     */
+    public static function isKeyValid($key)
+    {
+        return !empty($key) && is_string($key);
+    }
+
+    /**
+     * Return true if value is not empty and is a number or a boolean or a string,
+     * otherwise return false
+     * @param $value
+     * @return bool
+     */
+    public static function isValueValid($value)
+    {
+        return (!empty($value) && (is_numeric($value) || is_bool($value) || is_string($value)));
+    }
+}

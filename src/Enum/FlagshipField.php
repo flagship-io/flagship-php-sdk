a741cb267b409e1315b61a0dd4b59eaaa35d59be
Merveille KITOKO <merkitoko@gmail.com>
2021-04-22 18:04:56 +0100

    Add FlagshipField class

---
+++ src/Enum/FlagshipField.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Flagship\Enum;
+
+/**
+ * A set of constant of Decision api item
+ * @package Flagship\Enum
+ */
+class FlagshipField
+{
+
+    const FIELD_MODIFICATIONS = 'modifications';
+    const FIELD_CAMPAIGNS = 'campaigns';
+    const FIELD_VALUE = 'value';
+    const FIELD_REFERENCE = 'reference';
+    const FIELD_VARIATION = 'variation';
+    const FIELD_VARIATION_GROUP_ID = 'variationGroupId';
+    const FIELD_ID = 'id';
+    const FIELD_CAMPAIGN_ID = 'campaignId';
+    const FIELD_VARIATION_ID = 'variationId';
+    const FIELD_IS_REFERENCE = 'isReference';
+}

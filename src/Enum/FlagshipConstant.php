cfca43298039c193f5bf564b0e63754e0ad52507
Merveille KITOKO <merkitoko@gmail.com>
2021-04-23 20:06:32 +0100

    Added New Constants in FlagshipConstant

--- src/Enum/FlagshipConstant.php
+++ src/Enum/FlagshipConstant.php
@@ -32,4 +32,24 @@ class FlagshipConstant
      * SDK version
      */
     const SDK_VERSION = "v1";
+
+    //Message Error
+    const INITIALIZATION_PARAM_ERROR        = "Params 'envId' and 'apiKey' must not be null or empty.";
+    const ERROR                          = "error";
+    const CONTEXT_PARAM_ERROR           = "params 'key' must be a non null String, and 'value' must be one of the
+        following types : String, Number, Boolean, JsonObject, JsonArray.";
+    const GET_MODIFICATION_CAST_ERROR    = "Modification for key '%s' has a different type. Default value is returned.";
+    const GET_MODIFICATION_MISSING_ERROR = "No modification for key '%s'. Default value is returned.";
+    const GET_MODIFICATION_KEY_ERROR     = "Key '%s' must not be null. Default value is returned.";
+    const GET_MODIFICATION_INFO_ERROR    = "No modification for key '%s'.";
+
+    //Messages Info
+    const SDK_STARTED_INFO = "Flagship SDK (version: %s) READY";
+
+    //Process
+    const PROCESS                       = 'process';
+    const PROCESS_INITIALIZATION        = 'INITIALIZATION';
+    const PROCESS_UPDATE_CONTEXT        = 'UPDATE CONTEXT';
+    const PROCESS_GET_MODIFICATION      = 'GET MODIFICATION';
+    const PROCESS_GET_MODIFICATION_INFO = 'GET MODIFICATION INFO';
 }

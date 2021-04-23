75c674d5f0d4c95ee688c6f3359f8743f8554a0d
Merveille KITOKO <merkitoko@gmail.com>
2021-04-23 17:44:45 +0100

    Implement Flagship class singleton pattern and add method isReady,start, getStatus, etc

--- src/Flagship.php
+++ src/Flagship.php
@@ -2,7 +2,140 @@
 
 namespace Flagship;
 
+use Flagship\Enum\FlagshipConstant;
+use Flagship\Enum\FlagshipStatus;
+use Flagship\Traits\LogTrait;
+
+/**
+ * Flagship main singleton.
+ */
 class Flagship
 {
+    use LogTrait;
 
+    /**
+     * @var Flagship
+     */
+    private static $instance;
+    /**
+     * @var FlagshipConfig
+     */
+    private $config;
+    /**
+     * @var int
+     */
+    private $status = FlagshipStatus::NOT_READY;
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * Flagship singleton instance
+     *
+     * @return Flagship
+     */
+    protected static function instance()
+    {
+        if (!self::$instance) {
+            self::$instance = new Flagship();
+        }
+        return self::$instance;
+    }
+
+
+    /**
+     * Start the flagship SDK, with a custom configuration implementation
+     *
+     * @param $envId : Environment id provided by Flagship.
+     * @param $apiKey : Secure api key provided by Flagship.
+     * @param FlagshipConfig|null $config : SDK configuration. @see FlagshipConfig
+     */
+    public static function start($envId, $apiKey, FlagshipConfig $config = null)
+    {
+        $flagship = self::instance();
+        if (!$config) {
+            $config = new FlagshipConfig($envId, $apiKey);
+        }
+        $config->setEnvId($envId);
+        $config->setApiKey($apiKey);
+
+        if (empty($envId) || empty($apiKey)) {
+            $flagship->logError(
+                $config->getLogManager(),
+                FlagshipConstant::INITIALIZATION_PARAM_ERROR,
+                [FlagshipConstant::PROCESS => FlagshipConstant::PROCESS_INITIALIZATION]
+            );
+        }
+
+        $flagship->setConfig($config);
+
+        if (self::isReady()) {
+            $flagship->logInfo(
+                $config->getLogManager(),
+                sprintf(FlagshipConstant::SDK_STARTED_INFO, FlagshipConstant::SDK_VERSION),
+                [FlagshipConstant::PROCESS => FlagshipConstant::PROCESS_INITIALIZATION]
+            );
+            self::instance()->setStatus(FlagshipStatus::READY);
+        } else {
+            self::instance()->setStatus(FlagshipStatus::NOT_READY);
+        }
+    }
+
+    /**
+     * Return true if the SDK is properly initialized, otherwise return false
+     *
+     * @return bool
+     */
+    public static function isReady()
+    {
+        if (self::$instance && self::$instance->config) {
+            $envId = self::$instance->config->getEnvId();
+            $apiKey = self::$instance->config->getApiKey();
+            return !empty($envId) && !empty($apiKey);
+        }
+        return false;
+    }
+
+    /**
+     * Return the current config set by the customer and used by the SDK.
+     *
+     * @return FlagshipConfig
+     */
+    public static function getConfig()
+    {
+        return self::instance()->config;
+    }
+
+    /**
+     * @param FlagshipConfig $config
+     * @return Flagship
+     */
+    protected function setConfig($config)
+    {
+        $this->config = $config;
+        return $this;
+    }
+
+    /**
+     * Return current status of Flagship SDK.
+     * e.g. FlagshipStatus::READY or FlagshipStatus::NOT_READY
+     *
+     * @return int
+     */
+    public static function getStatus()
+    {
+        return self::instance()->status;
+    }
+
+    /**
+     * Set Flagship SDK status
+     * @param int $status   FlagshipStatus::READY or FlagshipStatus::NOT_READY
+     * @return Flagship
+     */
+    protected function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
 }
---
+++ tests/FlagshipTest.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Flagship;
+
+use Flagship\Enum\FlagshipConstant;
+use Flagship\Enum\FlagshipStatus;
+use Flagship\Utils\Utils;
+use PHPUnit\Framework\TestCase;
+
+class FlagshipTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        //Set Flagship singleton to null
+        $instanceMethod = Utils::getMethod("Flagship\Flagship", 'instance');
+        $instance = $instanceMethod->invoke(null);
+        if ($instance instanceof  Flagship) {
+            Utils::setPrivateProperty($instance, 'instance', null);
+        }
+    }
+
+    public function testInstance()
+    {
+        $instanceMethod = Utils::getMethod("Flagship\Flagship", 'instance');
+        $instance1 = $instanceMethod->invoke(null);
+        $instance2 = $instanceMethod->invoke(null);
+
+        //Test static method instance return an instance of Flagship\Flagship
+        $this->assertInstanceOf("Flagship\Flagship", $instance1);
+        $this->assertInstanceOf("Flagship\Flagship", $instance1);
+
+        //Test static method instance return singleton of Flagship\Flagship
+        $this->assertSame($instance1, $instance2);
+    }
+
+    public function testStart()
+    {
+        //Test Start Flagship
+        $envId = "end_id";
+        $apiKey = "apiKey";
+        $config = new FlagshipConfig($envId, $apiKey);
+        Flagship::start($envId, $apiKey, $config);
+        $this->assertSame($config, Flagship::getConfig());
+        $this->assertTrue(Flagship::isReady());
+        $this->assertSame(FlagshipStatus::READY, Flagship::getStatus());
+    }
+
+    public function testStartWithoutConfig()
+    {
+        //Test Start Flagship
+        $envId = "end_id";
+        $apiKey = "apiKey";
+        Flagship::start($envId, $apiKey);
+        $this->assertInstanceOf('Flagship\FlagshipConfig', Flagship::getConfig());
+
+        $this->assertSame($envId, Flagship::getConfig()->getEnvId());
+        $this->assertSame($apiKey, Flagship::getConfig()->getApiKey());
+
+        $this->assertTrue(Flagship::isReady());
+        $this->assertSame(FlagshipStatus::READY, Flagship::getStatus());
+    }
+
+    public function testStartWithLog()
+    {
+        //Test Start Flagship
+        $envId = "end_id";
+        $apiKey = "apiKey";
+        $config = new FlagshipConfig($envId, $apiKey);
+        $flagshipStub = $this->getMockBuilder(
+            'Flagship\Flagship'
+        )->setMethods(['logInfo','logError'])->disableOriginalConstructor()->getMock();
+
+        $instanceMethod = Utils::getMethod("Flagship\Flagship", 'instance');
+        $instance = $instanceMethod->invoke(null);
+
+        Utils::setPrivateProperty($instance, 'instance', $flagshipStub);
+
+        $flagshipStub->expects($this->once())->method('logInfo')
+            ->with(
+                $config->getLogManager(),
+                sprintf(FlagshipConstant::SDK_STARTED_INFO, FlagshipConstant::SDK_VERSION),
+                [FlagshipConstant::PROCESS => FlagshipConstant::PROCESS_INITIALIZATION]
+            );
+
+        $flagshipStub->expects($this->never())->method('logError');
+
+        Flagship::start($envId, $apiKey, $config);
+
+        $this->assertSame($config, Flagship::getConfig());
+        $this->assertTrue(Flagship::isReady());
+        $this->assertSame(FlagshipStatus::READY, Flagship::getStatus());
+    }
+
+    public function testStartFailed()
+    {
+        //Test Start Flagship failed with empty envKey
+        $envId = "";
+        $apiKey = "apiKey";
+        $config = new FlagshipConfig($envId, $apiKey);
+        Flagship::start($envId, $apiKey, $config);
+        $this->assertSame($config, Flagship::getConfig());
+        $this->assertFalse(Flagship::isReady());
+        $this->assertSame(FlagshipStatus::NOT_READY, Flagship::getStatus());
+
+        //Test Start Flagship failed with empty apiKey
+        $envId = "envId";
+        $apiKey = "";
+        $config = new FlagshipConfig($envId, $apiKey);
+        Flagship::start($envId, $apiKey, $config);
+        $this->assertSame($config, Flagship::getConfig());
+        $this->assertFalse(Flagship::isReady());
+        $this->assertSame(FlagshipStatus::NOT_READY, Flagship::getStatus());
+
+        //Test Start Flagship failed with empty apiKey
+        $envId = "";
+        $apiKey = "";
+        $config = new FlagshipConfig($envId, $apiKey);
+        Flagship::start($envId, $apiKey, $config);
+        $this->assertSame($config, Flagship::getConfig());
+        $this->assertFalse(Flagship::isReady());
+        $this->assertSame(FlagshipStatus::NOT_READY, Flagship::getStatus());
+    }
+
+    public function testStartFailedWithLog()
+    {
+        $envId = null;
+        $apiKey = "apiKey";
+
+        $config = new FlagshipConfig($envId, $apiKey);
+
+        $flagshipStub = $this->getMockBuilder('Flagship\Flagship')
+            ->setMethods(['logInfo','logError'])
+            ->disableOriginalConstructor()->getMock();
+
+        $instanceMethod = Utils::getMethod("Flagship\Flagship", 'instance');
+        $instance = $instanceMethod->invoke(null);
+
+        Utils::setPrivateProperty($instance, 'instance', $flagshipStub);
+
+        $flagshipStub->expects($this->once())->method('logError')
+            ->with(
+                $config->getLogManager(),
+                FlagshipConstant::INITIALIZATION_PARAM_ERROR,
+                [FlagshipConstant::PROCESS => FlagshipConstant::PROCESS_INITIALIZATION]
+            );
+        $flagshipStub->expects($this->never())->method('logInfo');
+
+        Flagship::start($envId, $apiKey, $config);
+        $this->assertSame($config, Flagship::getConfig());
+        $this->assertFalse(Flagship::isReady());
+        $this->assertSame(FlagshipStatus::NOT_READY, Flagship::getStatus());
+    }
+
+    public function testGetStatus()
+    {
+        //Test Status default is NO_READY
+        $this->assertSame(FlagshipStatus::NOT_READY, Flagship::getStatus());
+
+        //Test FlagshipConfig is null
+        $this->assertFalse(Flagship::isReady());
+
+        //Test Start Flagship
+        $envId = "end_id";
+        $apiKey = "apiKey";
+        $config = new FlagshipConfig($envId, $apiKey);
+        Flagship::start($envId, $apiKey, $config);
+        $this->assertSame($config, Flagship::getConfig());
+        $this->assertTrue(Flagship::isReady());
+        $this->assertSame(FlagshipStatus::READY, Flagship::getStatus());
+    }
+
+    public function testIsReady()
+    {
+        //Test Flagship instance is null
+        $this->assertFalse(Flagship::isReady());
+    }
+}

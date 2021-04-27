1429a3dbf62ccafe5a735e29c23f67265ccca4ee
Merveille KITOKO <merkitoko@gmail.com>
2021-04-27 18:50:54 +0100

    [refactor] HttpClient increase performance

---
+++ demo/index.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * Modifications
+ *{
+    "key": "reference",
+    "key2": 1,
+    "key3": 3,
+    "key4": 4
+* }
+
+ * Toggle
+ * btnColor: “#EE3300” (string)
+ * background: “bleu ciel” (string)
+
+ * */
+
+use Flagship\Flagship;
+
+require __dir__ . '/../vendor/autoload.php';
+
+Flagship::start('envId', 'apiKey');
+$visitor = Flagship::newVisitor('visitor_1');
+if ($visitor) {
+    $startTime = microtime(true);
+    $visitor->synchronizedModifications();
+    $endTime = microtime(true);
+
+    echo "key = background, value = " .  $visitor->getModification('background', 'white') . "\n";
+    echo "key = key, value = " . $visitor->getModification('key', 'key') . "\n";
+    echo 'sync time ' . ($endTime - $startTime) . "\n";
+}

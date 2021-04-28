f56b34d58dffa31f4283a65e0293dc9025353626
Merveille KITOKO <merkitoko@gmail.com>
2021-04-28 18:03:48 +0100

    add new constants in FlagshipConstant class

--- src/Enum/FlagshipConstant.php
+++ src/Enum/FlagshipConstant.php
@@ -24,6 +24,8 @@ class FlagshipConstant
      * Decision api base url
      */
     const BASE_API_URL = "https://decision.flagship.io/v2";
+    const URL_CAMPAIGNS      = 'campaigns';
+    const URL_ACTIVATE_MODIFICATION = 'activate';
 
 
     const EXPOSE_ALL_KEYS = "exposeAllKeys";
@@ -34,17 +36,18 @@ class FlagshipConstant
     const SDK_VERSION = "v1";
 
     //Message Error
-    const INITIALIZATION_PARAM_ERROR        = "Params 'envId' and 'apiKey' must not be null or empty.";
+    const INITIALIZATION_PARAM_ERROR     = "Params 'envId' and 'apiKey' must not be null or empty.";
     const ERROR                          = "error";
-    const CONTEXT_PARAM_ERROR           = "params 'key' must be a non null String, and 'value' must be one of the
+    const CONTEXT_PARAM_ERROR            = "params 'key' must be a non null String, and 'value' must be one of the
         following types : String, Number, Boolean, JsonObject, JsonArray.";
     const GET_MODIFICATION_CAST_ERROR    = "Modification for key '%s' has a different type. Default value is returned.";
     const GET_MODIFICATION_MISSING_ERROR = "No modification for key '%s'. Default value is returned.";
-    const GET_MODIFICATION_KEY_ERROR     = "Key '%s' must not be null. Default value is returned.";
-    const GET_MODIFICATION_INFO_ERROR    = "No modification for key '%s'.";
+    const GET_MODIFICATION_KEY_ERROR = "Key '%s' must not be null. Default value is returned.";
+    const GET_MODIFICATION_ERROR     = "No modification for key '%s'.";
 
     //Messages Info
     const SDK_STARTED_INFO = "Flagship SDK (version: %s) READY";
+    const FLAGSHIP_SDK="Flagship SDK";
 
     //Process
     const PROCESS                       = 'process';
@@ -52,5 +55,13 @@ class FlagshipConstant
     const PROCESS_UPDATE_CONTEXT        = 'UPDATE CONTEXT';
     const PROCESS_GET_MODIFICATION      = 'GET MODIFICATION';
     const PROCESS_GET_MODIFICATION_INFO = 'GET MODIFICATION INFO';
-    const NEW_VISITOR                    = 'NEW VISITOR';
+    const PROCESS_NEW_VISITOR           = 'NEW VISITOR';
+    const PROCESS_ACTIVE_MODIFICATION   = 'ACTIVE MODIFICATION';
+
+    //Api items
+
+    const CUSTOMER_ENV_ID           = "cid";
+    const VISITOR_ID                = "vid";
+    const VARIATION_GROUP_ID        = "caid";
+    const VARIATION_ID              = "vaid";
 }

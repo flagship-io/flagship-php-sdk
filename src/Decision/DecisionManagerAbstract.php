516adabda14a211603c8bb4b2b9aee8087e020cf
Merveille KITOKO <merkitoko@gmail.com>
2021-04-29 17:04:33 +0100

    add DecisionManagerAbstract and DecisionManagerInterface

---
+++ src/Decision/DecisionManagerAbstract.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Flagship\Decision;
+
+
+use Flagship\Utils\HttpClientInterface;
+use Flagship\Visitor;
+
+abstract class DecisionManagerAbstract implements DecisionManagerInterface
+{
+    /**
+     * @var HttpClientInterface
+     */
+    protected $httpClient;
+
+    /**
+     * ApiManager constructor.
+     *
+     * @param HttpClientInterface $httpClient
+     */
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    /**
+     * @return HttpClientInterface
+     */
+    public function getHttpClient()
+    {
+        return $this->httpClient;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    abstract public function getCampaigns(Visitor $visitor);
+
+    /**
+     * @inheritDoc
+     */
+    abstract public function getModifications($campaigns);
+}

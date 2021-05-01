351801e824ea8818aa1b59caf1488e56040b5c5a
Merveille KITOKO <merkitoko@gmail.com>
2021-05-01 17:27:30 +0100

    add documentation in HitType class

--- src/Enum/HitType.php
+++ src/Enum/HitType.php
@@ -6,9 +6,38 @@ namespace Flagship\Enum;
 
 class HitType
 {
+    /**
+     * User has seen a URL
+     *
+     * @var string
+     */
     const PAGE_VIEW="PAGEVIEW";
+
+    /**
+     * User has seen a screen.
+     *
+     * @var string
+     */
     const SCREEN_VIEW = "SCREENVIEW";
+
+    /**
+     * User has made a transaction.
+     *
+     * @var string
+     */
     const TRANSACTION = "TRANSACTION";
+
+    /**
+     * Item bought in a transaction.
+     *
+     * @var string
+     */
     const ITEM = "ITEM";
+
+    /**
+     * User has made a specific action.
+     *
+     * @var string
+     */
     const EVENT = "EVENT";
 }

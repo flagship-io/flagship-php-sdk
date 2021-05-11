36d904a5cc390f482b1c67bbc778d4b9ef5fa760
Merveille KITOKO <merkitoko@gmail.com>
2021-05-11 16:47:44 +0100

    add getModification and getModificationInfo method in FlagController class

--- example/app/Http/Controllers/FlagController.php
+++ example/app/Http/Controllers/FlagController.php
@@ -2,71 +2,56 @@
 
 namespace App\Http\Controllers;
 
-use Flagship\Flagship;
+use App\Casts\TypeCastInterface;
+use App\Rules\CheckBoolean;
+use App\Rules\TypeCheck;
 use Flagship\Visitor;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 
 class FlagController extends Controller
 {
-    /**
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function getModification($key, Request $request, Visitor $visitor)
+
+    public function getModification($key, Request $request, Visitor $visitor, TypeCastInterface $typeCast)
     {
-        $data = $this->validate($request, [
-            'type' => ['required','string', Rule::in(['string', 'bool', 'float', 'int', 'bool', 'double', 'long'])],
-            'activate' => 'required|boolean',
-            'defaultValue' => ['required', function ($input, $value, $fails) use ($request) {
-                $type = $request->get('type');
+        try {
+            $data = $this->validate($request, [
+                'type' => ['required', 'string', Rule::in(['string',
+                    'bool', 'float',
+                    'int', 'bool', 'double', 'long'])],
+                'activate' => ['required', new CheckBoolean()],
+                'defaultValue' => ['required', new TypeCheck($request->get('type'))]
+            ]);
 
-                $check = false;
-                switch ($type) {
-                    case 'bool':
-                        $check = null !== filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
-                        break;
-                    case 'double':
-                    case 'long':
-                    case 'float':
-                    case 'int':
-                        $check = is_numeric($value);
-                        break;
-                    default:
-                        $check = is_string($value) ;
-                }
-                if (!$check) {
-                    $fails('The ' . $input . ' is not ' . $type);
-                }
-            }]
-        ]);
+            $defaultValue = $typeCast->castToType($data['defaultValue'], $data['type']);
 
-        $defaultValue = $data['defaultValue'];
-
-        switch ($data['type']) {
-            case 'bool':
-                $defaultValue = (bool)$defaultValue;
-                break;
-            case 'double':
-            case 'long':
-            case 'float':
-                $defaultValue = (float)$defaultValue;
-                break;
-            case 'int':
-                $defaultValue = (int)$defaultValue;
-                break;
+            $modificationValue = $visitor->getModification($key, $defaultValue, $data['activate']);
+            $result = [
+                "value" => $modificationValue,
+                'error' => null
+            ];
+            return response()->json($result);
+        } catch (ValidationException $exception) {
+            return response()->json(['error' => $exception->errors()]);
         }
-
-        return response()->json($visitor->getModification($key, $defaultValue, $data['activate']));
     }
 
     public function getModificationInfo($key, Visitor $visitor)
     {
+        $reponse = $visitor->getModificationInfo($key);
+        if (!$reponse) {
+            return response()->json(['error' => 'Failed']);
+        }
         return response()->json($visitor->getModificationInfo($key));
     }
 
     public function activeModification($key, Visitor $visitor)
     {
-        $visitor->activateModification($key);
-        return response()->json(null);
+        $check = $visitor->activateModification($key);
+        if (!$check) {
+            return response()->json(['error' => 'Failed']);
+        }
+        return response()->json(['successful operation']);
     }
 }

1d0a6fc60871a1f1b8f2360653a1c62d7953fca2
Merveille KITOKO <merkitoko@gmail.com>
2021-05-11 23:53:10 +0100

    add CheckFlagshipSession to check out a flagship session is started

---
+++ example/app/Http/Middleware/CheckFlagshipSession.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Log;
+
+class CheckFlagshipSession
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $message = ['error' => 'First, set your Flagship Environment ID & API Key'];
+        if (!$request->hasSession()) {
+            Log::error("Flagship", $message);
+            return response()->json($message);
+        }
+        if (!$request->session()->has('flagshipConfig')) {
+            Log::error("Flagship", $message);
+            return response()->json($message);
+        }
+        return $next($request);
+    }
+}

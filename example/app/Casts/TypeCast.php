8035fdacb87160a90ebcae1f628eda4d8745d022
Merveille KITOKO <merkitoko@gmail.com>
2021-05-11 16:50:44 +0100

    add TypeCast class and TypeCastInterface

---
+++ example/app/Casts/TypeCast.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Casts;
+
+class TypeCast implements TypeCastInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function castToType($defaultValue, $type)
+    {
+        $value = $defaultValue;
+        switch ($type) {
+            case 'bool':
+                $value = filter_var($defaultValue, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+                break;
+            case 'double':
+            case 'long':
+            case 'float':
+                $value = (float)$defaultValue;
+                break;
+            case 'int':
+                $value = (int)$defaultValue;
+                break;
+        }
+        return $value;
+    }
+}

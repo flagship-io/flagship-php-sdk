8b494c605846d45daae7bddc8390d8f5c99c2eb0
Merveille KITOKO <merkitoko@gmail.com>
2021-05-11 16:45:11 +0100

    add TypeCheck validation rule

---
+++ example/app/Rules/TypeCheck.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class TypeCheck implements Rule
+{
+    /**
+     * @var string
+     */
+    private $type;
+    /**
+     * @var string
+     */
+    private $attribute;
+
+    /**
+     * TypeCheck constructor.
+     * @param string $type
+     */
+    public function __construct($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function passes($attribute, $value)
+    {
+        $this->attribute = $attribute;
+        $check = false;
+        switch ($this->type) {
+            case 'bool':
+                $check = null !== filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+                break;
+            case 'double':
+            case 'long':
+            case 'float':
+            case 'int':
+                $check = is_numeric($value);
+                break;
+            default:
+                $check = is_string($value) ;
+        }
+        return $check;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function message()
+    {
+        return 'The ' . $this->attribute . ' is not ' . $this->type;
+    }
+}

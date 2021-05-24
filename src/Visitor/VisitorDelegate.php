489d3db4c74170e9d25c446b9593a3d4b66029e1
Merveille KITOKO <merkitoko@gmail.com>
2021-05-24 18:00:43 +0100

    add VisitorDelegate

--- src/Visitor/VisitorDelegate.php
+++ src/Visitor/VisitorDelegate.php
@@ -4,31 +4,23 @@ namespace Flagship\Visitor;
 
 use Flagship\Hit\HitAbstract;
 use Flagship\Utils\ConfigManager;
-use Flagship\Utils\ContainerInterface;
 
 class VisitorDelegate extends VisitorAbstract
 {
     /**
      * Create a new VisitorDelegate.
      *
-     * @param ContainerInterface $dependencyIContainer
      * @param ConfigManager $configManager
      * @param string $visitorId : visitor unique identifier.
      * @param array $context : visitor context. e.g: ["age"=>42, "isVip"=>true, "country"=>"UK"]
      */
-    public function __construct(
-        ContainerInterface $dependencyIContainer,
-        ConfigManager $configManager,
-        $visitorId,
-        array $context = []
-    ) {
-        $this->setDependencyIContainer($dependencyIContainer);
+    public function __construct(ConfigManager $configManager, $visitorId, array $context = [])
+    {
         $this->setConfig($configManager->getConfig());
         $this->setVisitorId($visitorId);
-        $this->setContext($context);
+        $this->updateContextCollection($context);
         $this->setConfigManager($configManager);
     }
-
     /**
      * @inheritDoc
      */

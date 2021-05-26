82962169e9f06afee93fe8441c63ea3ce427a22b
Merveille KITOKO <merkitoko@gmail.com>
2021-05-26 15:24:56 +0100

    refactor getModification method add array as allow type

--- src/Visitor/VisitorInterface.php
+++ src/Visitor/VisitorInterface.php
@@ -43,13 +43,13 @@ interface VisitorInterface
      * key or if the stored value type and default value type do not match, default value will be returned.
      *
      * @param string              $key          : key associated to the modification.
-     * @param string|bool|numeric $defaultValue : default value to return.
+     * @param string|bool|numeric|array $defaultValue : default value to return.
      * @param bool                $activate     : Set this parameter to true to automatically
      *                                          report on our server that the
      *                                          current visitor has seen this modification. It is possible to call
      *                                          activateModification() later.
 
-     * @return string|bool|numeric : modification value or default value.
+     * @return string|bool|numeric|array : modification value or default value.
      */
     public function getModification($key, $defaultValue, $activate = false);
 

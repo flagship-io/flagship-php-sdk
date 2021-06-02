77b25e28168d51ba25dd7b9f5fc09f5476f24f77
Merveille KITOKO <merkitoko@gmail.com>
2021-06-02 16:50:56 +0100

    add demo with 2 docker container sharing same bucketing file

---
+++ demo/index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Flagship\Enum\DecisionMode;
+use Flagship\Flagship;
+use Flagship\FlagshipConfig;
+
+$envId  =  getenv('FLAGSHIP_ENV_ID');
+$apiKey = getenv('FLAGSHIP_API_KEY');
+$config = new FlagshipConfig();
+$config->setDecisionMode(DecisionMode::BUCKETING);
+
+Flagship::start($envId, $apiKey, $config);
+
+$visitor_Id = "visitor_1";
+$context = [
+    "isPhp" => true
+];
+
+$visitor = Flagship::newVisitor($visitor_Id, $context);
+
+while (true) {
+    echo "============================================================" .  PHP_EOL;
+    echo 'visitor context';
+    echo json_encode($context);
+    echo PHP_EOL;
+    $visitor->synchronizedModifications();
+     print_r($visitor->getModifications());
+    sleep(10);
+}

4a5048d116b5774a1e495719db786deb6efd489c
Merveille KITOKO <merkitoko@gmail.com>
2021-06-04 18:24:28 +0100

    add checkAndUpdateConfigField  method in BucketingPolling

--- src/Decision/BucketingPolling.php
+++ src/Decision/BucketingPolling.php
@@ -16,20 +16,48 @@ class BucketingPolling
      * @var HttpClient
      */
     private $httpClient;
+    /**
+     * @var null
+     */
+    private $configFile;
 
     /**
      * @param string $envId
      * @param int $pollingInterval
      * @param HttpClientInterface $httpClient
      * @param string $bucketingDirectory
+     * @param string $configFile
      */
-    public function __construct($envId, $pollingInterval, HttpClientInterface $httpClient, $bucketingDirectory = null)
-    {
+    public function __construct(
+        $envId,
+        $pollingInterval,
+        HttpClientInterface $httpClient,
+        $bucketingDirectory = null,
+        $configFile = null
+    ) {
         $this->envId = $envId;
         $this->pollingInterval = $pollingInterval;
         $this->httpClient = $httpClient;
         $this->bucketingDirectory = $bucketingDirectory ? __DIR__ . '/../../../../../' .
             $bucketingDirectory : __DIR__ . FlagshipConstant::BUCKETING_DIRECTORY;
+        $this->configFile = $configFile;
+    }
+
+    /**
+     *
+     */
+    private function checkAndUpdateConfigField()
+    {
+        if (file_exists($this->configFile)) {
+            $fileContent = file_get_contents($this->configFile);
+            $configArray = json_decode($fileContent, true);
+            if (!empty($configArray['envId'])) {
+                $this->envId = $configArray['envId'];
+            }
+            if (isset($configArray['pollingInterval']) && is_numeric($configArray['pollingInterval'])) {
+                $this->pollingInterval = $configArray['pollingInterval'];
+            }
+        }
     }
 
     public function startPolling()
@@ -40,6 +68,7 @@ class BucketingPolling
         }
         do {
             try {
+                $this->checkAndUpdateConfigField();
                 echo 'Polling start' . PHP_EOL;
                 $url = sprintf(FlagshipConstant::BUCKETING_API_URL, $this->envId);
                 $response = $this->httpClient->get($url);

9991be2095009ec7c4a1696ab94d555db80e2604
Merveille KITOKO <merkitoko@gmail.com>
2021-06-07 17:09:12 +0100

    add updateConsent route

--- example/routes/web.php
+++ example/routes/web.php
@@ -34,15 +34,23 @@ $router->group(
         $router->put(
             '/context/{key}',
             [
-                'middleware' => 'CheckFlagshipSession|flagshipVisitor',
+                'middleware' => 'CheckFlagshipSession|startFlagship|flagshipVisitor',
                 'uses' => 'VisitorController@updateContext'
+            ]
+        );
+
+        $router->put(
+            '/consent',
+            [
+                'middleware' => 'CheckFlagshipSession|flagshipVisitor',
+                'uses' => 'VisitorController@updateConsent'
             ]
         );
     }
 );
 
 $router->group(
-    ['prefix' => 'flag', 'middleware' => ['CheckFlagshipSession','flagshipVisitor']],
+    ['prefix' => 'flag', 'middleware' => ['CheckFlagshipSession','startFlagship','flagshipVisitor']],
     function () use ($router) {
         $router->get('/{key}/activate', 'FlagController@activeModification');
         $router->get('/{key}/info', 'FlagController@getModificationInfo');
@@ -51,7 +59,7 @@ $router->group(
 );
 
 $router->group(
-    ['prefix' => 'hit', 'middleware' => ['CheckFlagshipSession','flagshipVisitor']],
+    ['prefix' => 'hit', 'middleware' => ['CheckFlagshipSession','startFlagship', 'flagshipVisitor']],
     function () use ($router) {
         $router->post('/', 'HitController@sendHit');
     }

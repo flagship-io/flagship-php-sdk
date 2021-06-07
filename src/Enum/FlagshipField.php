e4b8edef4e8b4789b4fcce81e91b4c21b77dad29
Merveille KITOKO <merkitoko@gmail.com>
2021-06-07 13:12:07 +0100

    add new constants in FlagshipField.php

--- src/Enum/FlagshipField.php
+++ src/Enum/FlagshipField.php
@@ -10,22 +10,27 @@ namespace Flagship\Enum;
 class FlagshipField
 {
 
-    const FIELD_MODIFICATIONS      = 'modifications';
-    const FIELD_CAMPAIGNS          = 'campaigns';
-    const FIELD_VALUE              = 'value';
-    const FIELD_REFERENCE          = 'reference';
-    const FIELD_VARIATION          = 'variation';
+    const FIELD_MODIFICATIONS = 'modifications';
+    const FIELD_CAMPAIGNS = 'campaigns';
+    const FIELD_VALUE = 'value';
+    const FIELD_REFERENCE = 'reference';
+    const FIELD_VARIATION = 'variation';
     const FIELD_VARIATION_GROUP_ID = 'variationGroupId';
-    const FIELD_ID               = 'id';
-    const FIELD_CAMPAIGN_ID      = 'campaignId';
-    const FIELD_VARIATION_ID     = 'variationId';
-    const FIELD_IS_REFERENCE     = 'isReference';
-    const FIELD_KEY              = 'key';
+    const FIELD_ID = 'id';
+    const FIELD_CAMPAIGN_ID = 'campaignId';
+    const FIELD_VARIATION_ID = 'variationId';
+    const FIELD_IS_REFERENCE = 'isReference';
+    const FIELD_KEY = 'key';
     const FIELD_VARIATION_GROUPS = "variationGroups";
-    const FIELD_TARGETING        = 'targeting';
+    const FIELD_TARGETING = 'targeting';
     const FIELD_TARGETING_GROUPS = 'targetingGroups';
-    const FIELD_PANIC            = 'panic';
-    const FIELD_TARGETINGS       = 'targetings';
-    const FIELD_VARIATIONS       = "variations";
-    const FIELD_ALLOCATION       = 'allocation';
+    const FIELD_PANIC = 'panic';
+    const FIELD_TARGETINGS = 'targetings';
+    const FIELD_VARIATIONS = "variations";
+    const FIELD_ALLOCATION = 'allocation';
+    const FIELD_POLLING_INTERVAL = 'pollingInterval';
+    const FIELD_ENVIRONMENT_ID = "environmentId";
+    const FIELD_API_KEY = "apiKey";
+    const FIELD_TIMEOUT = "timeout";
+    const FIELD_LOG_LEVEL = "logLevel";
 }

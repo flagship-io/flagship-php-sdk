6210f65dfef7c23369d019b38b1c5ed65dc05976
Merveille KITOKO <merkitoko@gmail.com>
2021-06-07 17:02:52 +0100

    Refactor update method in EnvController

--- example/tests/Controller/EnvControllerTest.php
+++ example/tests/Controller/EnvControllerTest.php
@@ -11,10 +11,33 @@ class EnvControllerTest extends TestCase
         $data = [
             "environment_id" => "env_id",
             "api_key" => "api_key",
-            "timeout" => 2000
+            "timeout" => 2000,
+            "bucketing" => false,
+            "polling_interval" => 0
         ];
         $this->put('/env', $data);
         $this->assertJsonStringEqualsJsonString(json_encode($data), $this->response->getContent());
+
+        $data = [
+            "environment_id" => "env_id",
+            "api_key" => "api_key",
+            "timeout" => 2000,
+            "bucketing" => true,
+            "polling_interval" => 2000
+        ];
+        $this->put('/env', $data);
+        $this->assertJsonStringEqualsJsonString(json_encode($data), $this->response->getContent());
+
+        //Test validation
+        $data = [
+            "environment_id" => "env_id",
+            "api_key" => "api_key",
+            "timeout" => 2000,
+            "bucketing" => true,
+            "polling_interval" => null
+        ];
+        $this->put('/env', $data);
+        $this->assertJsonStringEqualsJsonString('{"error":{"polling_interval":["The polling interval must be a number."]}}', $this->response->getContent());
     }
 
 
@@ -23,7 +46,9 @@ class EnvControllerTest extends TestCase
         $data = [
             "environment_id" => "env_id",
             "api_key" => "api_key",
-            "timeout" => 2000
+            "timeout" => 2000,
+            "bucketing" => false,
+            "polling_interval" => 0
         ];
         $this->put('/env', $data);
         $this->get('/env');

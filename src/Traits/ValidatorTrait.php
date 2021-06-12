435f5a56cba49428bf4cb4aa371cc8b86cebcaf1
Merveille KITOKO <merkitoko@gmail.com>
2021-06-12 11:09:06 +0100

    add checkFlagshipContext method in ValidatorTrait.php

--- src/Traits/ValidatorTrait.php
+++ src/Traits/ValidatorTrait.php
@@ -9,6 +9,78 @@ trait ValidatorTrait
 {
     use LogTrait;
 
+    protected function isJsonObject($string)
+    {
+        $jsonObject = json_decode($string);
+
+        if ($jsonObject === null) {
+            return false;
+        }
+
+        $json = ltrim($string);
+
+        if (strpos($json, '{') === 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param string $type
+     * @param mixed $value
+     * @return bool
+     */
+    protected function checkType($type, $value)
+    {
+        switch ($type) {
+            case 'bool':
+                $check = null !== filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+                break;
+            case 'double':
+            case 'long':
+            case 'float':
+            case 'int':
+            case 'integer':
+                $check = is_numeric($value);
+                break;
+            default:
+                $check = is_string($value);
+        }
+        return $check;
+    }
+
+    protected function checkFlagshipContext($key, $value, FlagshipConfig $config)
+    {
+        if (!$this->isJsonObject($key)) {
+            return $value;
+        }
+
+        $flagshipConstant = json_decode($key, true);
+
+        if (
+            !isset($flagshipConstant['key'])
+            || !isset($flagshipConstant['type'])
+            || $flagshipConstant['key'] == "fs_client"
+            || $flagshipConstant['key'] == "fs_version"
+            || $flagshipConstant['key'] == "fs_users"
+        ) {
+            return  null;
+        }
+
+        if (!$this->checkType($flagshipConstant['type'], $value)) {
+            $this->logError(
+                $config,
+                sprintf(
+                    FlagshipConstant::FLAGSHIP_PREDEFINED_CONTEXT_ERROR,
+                    $flagshipConstant['key'],
+                    $flagshipConstant['type']
+                )
+            );
+            return null;
+        }
+        return $flagshipConstant;
+    }
+
     /**
      * Return true if key is not empty and is a string, otherwise return false
      *

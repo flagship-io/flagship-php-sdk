84fc7629be59139888cd89122659087cbf0790bc
Merveille KITOKO <merkitoko@gmail.com>
2021-06-14 18:56:03 +0100

    add testCheckType, testCheckFlagshipContext, testIsJsonObject and testIsNumeric test in ValidatorTraitTest.php

--- tests/Traits/ValidatorTraitTest.php
+++ tests/Traits/ValidatorTraitTest.php
@@ -2,6 +2,9 @@
 
 namespace Flagship\Traits;
 
+use Flagship\Config\BucketingConfig;
+use Flagship\Enum\FlagshipConstant;
+use Flagship\Enum\FlagshipContext;
 use Flagship\Utils\Utils;
 use PHPUnit\Framework\TestCase;
 
@@ -64,5 +67,155 @@ class ValidatorTraitTest extends TestCase
 
         //Test value is boolean
         $this->assertTrue($isValueValid->invokeArgs($validatorTraitMock, [false]));
+    }
+
+    public function testCheckType()
+    {
+        $validatorTraitMock = $this->getMockForTrait(
+            'Flagship\Traits\ValidatorTrait',
+            [],
+            "",
+            false,
+            true,
+            true
+        );
+
+        $checkType = Utils::getMethod($validatorTraitMock, "checkType");
+
+        // Value is empty
+        $this->assertTrue($checkType->invokeArgs($validatorTraitMock, ['bool', true]));
+        $this->assertTrue($checkType->invokeArgs($validatorTraitMock, ['bool', false]));
+        $this->assertFalse($checkType->invokeArgs($validatorTraitMock, ['bool', "abc"]));
+
+        $this->assertTrue($checkType->invokeArgs($validatorTraitMock, ['float', 1.5]));
+        $this->assertTrue($checkType->invokeArgs($validatorTraitMock, ['double', 1.5]));
+        $this->assertTrue($checkType->invokeArgs($validatorTraitMock, ['integer', 1]));
+        $this->assertFalse($checkType->invokeArgs($validatorTraitMock, ['integer', "abc"]));
+
+        $this->assertTrue($checkType->invokeArgs($validatorTraitMock, ['string', "abc"]));
+    }
+
+    public function testCheckFlagshipContext()
+    {
+        //Mock logManger
+        $logManagerStub = $this->getMockForAbstractClass(
+            'Psr\Log\LoggerInterface',
+            [],
+            "",
+            true,
+            true,
+            true,
+            ['error']
+        );
+
+        $validatorTraitMock = $this->getMockForTrait(
+            'Flagship\Traits\ValidatorTrait',
+            [],
+            "",
+            false,
+            true,
+            true
+        );
+
+        $checkFlagshipContext = Utils::getMethod($validatorTraitMock, "checkFlagshipContext");
+        $config = new BucketingConfig();
+        $config->setLogManager($logManagerStub);
+        $value = "linux";
+        $contexts = $checkFlagshipContext->invokeArgs($validatorTraitMock, ['item',$value, $config]);
+        $this->assertSame($contexts, $value);
+
+        $contexts = $checkFlagshipContext->invokeArgs(
+            $validatorTraitMock,
+            [FlagshipContext::FLAGSHIP_VERSION,$value, $config]
+        );
+        $this->assertNull($contexts);
+
+        $contexts = $checkFlagshipContext->invokeArgs(
+            $validatorTraitMock,
+            [FlagshipContext::FLAGSHIP_VISITOR,$value, $config]
+        );
+        $this->assertNull($contexts);
+
+        $contexts = $checkFlagshipContext->invokeArgs(
+            $validatorTraitMock,
+            [FlagshipContext::FLAGSHIP_CLIENT,$value, $config]
+        );
+        $this->assertNull($contexts);
+
+        $sdk = FlagshipConstant::FLAGSHIP_SDK;
+        $logManagerStub->expects($this->once())->method('error')
+            ->with("[$sdk] " .
+                sprintf(
+                    FlagshipConstant::FLAGSHIP_PREDEFINED_CONTEXT_ERROR,
+                    "sdk_osName",
+                    "string"
+                ));
+        $value = 1;
+        $contexts = $checkFlagshipContext->invokeArgs(
+            $validatorTraitMock,
+            [FlagshipContext::OS_NAME,$value, $config]
+        );
+        $this->assertNull($contexts);
+
+        $value = "mac";
+        $contexts = $checkFlagshipContext->invokeArgs(
+            $validatorTraitMock,
+            [FlagshipContext::OS_NAME,$value, $config]
+        );
+        $this->assertSame([ 'key' => "sdk_osName", "type" => "string"], $contexts);
+    }
+
+    public function testIsJsonObject()
+    {
+        $validatorTraitMock = $this->getMockForTrait(
+            'Flagship\Traits\ValidatorTrait',
+            [],
+            "",
+            false,
+            true,
+            true
+        );
+
+        $isJsonObject = Utils::getMethod($validatorTraitMock, "isJsonObject");
+
+        $this->assertFalse($isJsonObject->invokeArgs($validatorTraitMock, ["item"]));
+        $this->assertTrue($isJsonObject->invokeArgs($validatorTraitMock, ["{}"]));
+        $this->assertFalse($isJsonObject->invokeArgs($validatorTraitMock, ["[]"]));
+    }
+
+    public function testIsNumeric()
+    {
+        //Mock logManger
+        $logManagerStub = $this->getMockForAbstractClass(
+            'Psr\Log\LoggerInterface',
+            [],
+            "",
+            true,
+            true,
+            true,
+            ['error']
+        );
+
+        $validatorTraitMock = $this->getMockForTrait(
+            'Flagship\Traits\ValidatorTrait',
+            [],
+            "",
+            false,
+            true,
+            true
+        );
+        $itemName = "test";
+
+        $isNumeric = Utils::getMethod($validatorTraitMock, "isNumeric");
+        $config = new BucketingConfig();
+        $config->setLogManager($logManagerStub);
+        $this->assertTrue($isNumeric->invokeArgs($validatorTraitMock, [1, $itemName, $config]));
+
+        $sdk = FlagshipConstant::FLAGSHIP_SDK;
+        $logManagerStub->expects($this->once())->method('error')
+            ->with("[$sdk] " .
+                sprintf(FlagshipConstant::TYPE_ERROR, $itemName, 'numeric'));
+
+        $this->assertFalse($isNumeric->invokeArgs($validatorTraitMock, ["abc", $itemName, $config]));
     }
 }

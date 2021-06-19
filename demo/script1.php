54233899ffe26a2d0c0512ea60d0f114f03fe6fb
Merveille KITOKO <merkitoko@gmail.com>
2021-06-19 13:19:48 +0100

    add script1.php

---
+++ demo/script1.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Flagship\Config\BucketingConfig;
+use Flagship\Enum\FlagshipStatus;
+use Flagship\Flagship;
+
+$envId = getenv('FLAGSHIP_ENV_ID');
+$apiKey = getenv('FLAGSHIP_API_KEY');
+$bucketingDirectory = getenv("FLAGSHIP_BUCKETING_DIRECTORY");
+
+$config = new BucketingConfig();
+
+
+$config->setStatusChangedCallable(function ($status) {
+    if ($status === FlagshipStatus::READY) {
+        echo "SDK is ready";
+    }
+});
+
+$config->setBucketingDirectoryPath($bucketingDirectory);
+
+Flagship::start($envId, $apiKey, $config);
+
+$visitor_Id = "visitor_1";
+$context = [
+    "isPhp" => true
+];
+
+$visitor = Flagship::newVisitor($visitor_Id, $context);
+while (true) {
+    echo "============================================================" . PHP_EOL;
+    echo 'visitor context';
+    echo json_encode($context);
+    echo PHP_EOL;
+    if (!$visitor) {
+        break;
+    }
+    $visitor->synchronizedModifications();
+    print_r($visitor->getModifications());
+    sleep(10);
+}

233ca7d69b686e361c88759ff3efca29de297549
Merveille KITOKO <merkitoko@gmail.com>
2022-01-13 12:09:47 +0100

    add FlagTest

---
+++ tests/Flag/FlagTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Flagship\Flag;
+
+use Flagship\Model\FlagDTO;
+use PHPUnit\Framework\TestCase;
+
+class FlagTest extends TestCase
+{
+
+    public function testFlag()
+    {
+        $key = "key";
+        $defaultValue = "DefaultValue";
+        $flagDTO = new FlagDTO();
+        $flagDTO->setIsReference(true)
+            ->setVariationId("variationId")
+            ->setVariationGroupId('varGroupId')
+            ->setCampaignId("campaignId")
+            ->setKey($key)->setValue("value");
+
+        $metadata = new FlagMetadata(
+            $flagDTO->getCampaignId(),
+            $flagDTO->getVariationGroupId(),
+            $flagDTO->getVariationId(),
+            $flagDTO->getIsReference(),
+            ""
+        );
+        $visitorDelegateMock = $this->getMockForAbstractClass(
+            'Flagship\Visitor\VisitorAbstract',
+            ['getFlagValue','userExposed','getFlagMetadata'],
+            '',
+            false
+        );
+        $flag = new Flag($key, $visitorDelegateMock, $defaultValue, $metadata, $flagDTO);
+
+        $visitorDelegateMock->expects($this->exactly(2))->method('getFlagValue')->withConsecutive(
+            [ $key,
+            $defaultValue,
+            $flagDTO,
+            true],
+            [ $key,
+               $defaultValue,
+               $flagDTO,
+               false]
+        )->willReturn($flagDTO->getValue());
+
+        $value = $flag->value();
+        $this->assertEquals($value, $flagDTO->getValue());
+
+        $value = $flag->value(false);
+        $this->assertEquals($value, $flagDTO->getValue());
+
+        $this->assertTrue($flag->exists());
+
+        $visitorDelegateMock->expects($this->once())->method('userExposed')->with(
+            $key,
+            $flagDTO,
+            true
+        );
+
+        $flag->userExposed();
+
+        $visitorDelegateMock->expects($this->once())->method('getFlagMetadata')->with(
+            $key,
+            $metadata,
+            true
+        )->willReturn($metadata);
+
+        $metadataValue = $flag->getMetadata();
+
+        $this->assertSame($metadataValue, $metadata);
+    }
+
+    public function testFlagNull()
+    {
+        $key = "key";
+        $defaultValue = "DefaultValue";
+
+        $metadata = new FlagMetadata(
+            "",
+            "",
+            "",
+            "",
+            ""
+        );
+        $visitorDelegateMock = $this->getMockForAbstractClass(
+            'Flagship\Visitor\VisitorAbstract',
+            ['getFlagValue','userExposed','getFlagMetadata'],
+            '',
+            false
+        );
+        $flag = new Flag($key, $visitorDelegateMock, $defaultValue, $metadata, null);
+
+        $this->assertFalse($flag->exists());
+
+        $visitorDelegateMock->expects($this->never())->method('getFlagMetadata');
+
+        $metadataValue = $flag->getMetadata();
+
+        $this->assertSame($metadataValue, $metadata);
+    }
+
+    public function testExists()
+    {
+    }
+
+    public function testGetMetadata()
+    {
+    }
+}

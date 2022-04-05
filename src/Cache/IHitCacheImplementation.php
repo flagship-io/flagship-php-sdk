2bd00bb14c736989caf5e8ffb35cc6a824e401e5
Heihokon <neoshark22@gmail.com>
2022-04-05 16:37:36 +0100

    add IHitCacheImplementation.php

--- src/Cache/IHitCacheImplementation.php
+++ src/Cache/IHitCacheImplementation.php
@@ -12,5 +12,18 @@ interface IHitCacheImplementation
      */
     public function cacheHit($visitorId, array $data);
 
+    /**
+     * This method will be called to load hits corresponding to visitor ID from your database and trying to send them again in the background.
+     * Note: Hits older than 4H will be ignored
+     * @param string $visitorId
+     * @return array
+     */
     public function lookupHits($visitorId);
+
+    /**
+     * This method will be called to erase the visitor hits cache corresponding to visitor ID from your database.
+     * @param string $visitorId
+     * @return void
+     */
+    public function flushHits($visitorId);
 }

3563a7e0f5a97345c42a0e6ad177004be6ded549
Heihokon <neoshark22@gmail.com>
2022-04-05 16:36:53 +0100

    add IVisitorCacheImplementation.php

---
+++ src/Cache/IVisitorCacheImplementation.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Flagship\Cache;
+
+interface IVisitorCacheImplementation
+{
+    /**
+     * This method is called when the SDK needs to cache visitor information in your database.
+     * @param string $visitorId
+     * @param array $data
+     * @return void
+     */
+    public  function cacheVisitor($visitorId, array $data);
+
+    /**
+     * This method is called when the SDK needs to get the visitor information corresponding to visitor ID from your database.
+     * @param string $visitorId
+     * @return array
+     */
+    public function lookupVisitor($visitorId);
+
+    /**
+     * This method is called when the SDK needs to erase the visitor information corresponding to visitor ID in your database.
+     * @param string $visitorId
+     * @return void
+     */
+    public function flushVisitor($visitorId);
+}

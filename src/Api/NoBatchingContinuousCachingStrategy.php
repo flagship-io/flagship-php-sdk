68e056f6d4e8568a6899725370edc26b42541276
Heihokon <neoshark22@gmail.com>
2022-12-07 16:09:22 +0100

    add NoBatchingContinuousCachingStrategy class

---
+++ src/Api/NoBatchingContinuousCachingStrategy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Flagship\Api;
+
+use Flagship\Hit\Activate;
+use Flagship\Hit\HitAbstract;
+
+class NoBatchingContinuousCachingStrategy extends BatchingCachingStrategyAbstract
+{
+
+    protected function notConsent($visitorId)
+    {
+        // TODO: Implement notConsent() method.
+    }
+
+    protected function addHitInPoolQueue(HitAbstract $hit)
+    {
+        // TODO: Implement addHitInPoolQueue() method.
+    }
+
+    protected function addActivateHitInPoolQueue(Activate $hit)
+    {
+        // TODO: Implement addActivateHitInPoolQueue() method.
+    }
+
+    protected function sendActivateHit()
+    {
+        // TODO: Implement sendActivateHit() method.
+    }
+}

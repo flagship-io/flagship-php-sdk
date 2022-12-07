0cc32f96a8da2976dc73ce74d5e218b155bcae70
Heihokon <neoshark22@gmail.com>
2022-12-07 16:08:22 +0100

    add initStrategy method

--- src/Api/TrackingManagerAbstract.php
+++ src/Api/TrackingManagerAbstract.php
@@ -3,6 +3,8 @@
 namespace Flagship\Api;
 
 use Flagship\Config\FlagshipConfig;
+use Flagship\Enum\CacheStrategy;
+use Flagship\Hit\Activate;
 use Flagship\Hit\HitAbstract;
 use Flagship\Model\FlagDTO;
 use Flagship\Utils\HttpClientInterface;
@@ -12,7 +14,7 @@ use Flagship\Visitor\VisitorAbstract;
  * Class TrackingManagerAbstract
  * @package Flagship\Api
  */
-abstract class TrackingManagerAbstract
+abstract class TrackingManagerAbstract implements TrackingManagerInterface
 {
     /**
      * @var HttpClientInterface
@@ -20,13 +22,38 @@ abstract class TrackingManagerAbstract
     protected $httpClient;
 
     /**
+     * @var HitAbstract[]
+     */
+    protected $hitsPoolQueue;
+
+    /**
+     * @var Activate[]
+     */
+    protected $activatePoolQueue;
+    /**
+     * @var FlagshipConfig
+     */
+    protected $config;
+
+    /**
+     * @var BatchingCachingStrategyAbstract
+     */
+    protected $strategy;
+
+    /**
      * ApiManager constructor.
      *
+     * @param FlagshipConfig $config
      * @param HttpClientInterface $httpClient
      */
-    public function __construct(HttpClientInterface $httpClient)
+    public function __construct(FlagshipConfig $config, HttpClientInterface $httpClient)
     {
         $this->httpClient = $httpClient;
+        $this->config = $config;
+        $this->activatePoolQueue = [];
+        $this->hitsPoolQueue = [];
+        $this->strategy = $this->initStrategy();
+        $this->lookupHits();
     }
 
     /**
@@ -38,24 +65,28 @@ abstract class TrackingManagerAbstract
     }
 
     /**
-     * Send to server that this user has seen this modification
-     *
-     * @param VisitorAbstract $visitor
-     * @param FlagDTO $modification
-     * @return void
+     * @return BatchingCachingStrategyAbstract
      */
-    abstract public function sendActive(VisitorAbstract $visitor, FlagDTO $modification);
+    public function initStrategy(){
+        switch ($this->config->getCacheStrategy()){
+            case CacheStrategy::CONTINUOUS_CACHING:
+                $strategy = new BatchingContinuousCachingStrategy($this->config,$this->httpClient,
+                    $this->hitsPoolQueue, $this->activatePoolQueue);
+                break;
+            case CacheStrategy::PERIODIC_CACHING:
+                $strategy = new BatchingPeriodicCachingStrategy($this->config,$this->httpClient,
+                    $this->hitsPoolQueue, $this->activatePoolQueue);
+                break;
+            default:
+                $strategy = new NoBatchingContinuousCachingStrategy($this->config,$this->httpClient,
+                    $this->hitsPoolQueue, $this->activatePoolQueue);
+                break;
+        }
+        return $strategy;
+    }
 
-    /**
-     * @param  HitAbstract $hit
-     * @return void
-     */
-    abstract public function sendHit(HitAbstract $hit);
 
-    /**
-     * @param VisitorAbstract $visitor
-     * @param FlagshipConfig $config
-     * @return void
-     */
-    abstract public function sendConsentHit(VisitorAbstract $visitor, FlagshipConfig $config);
+    public function lookupHits(){
+
+    }
 }

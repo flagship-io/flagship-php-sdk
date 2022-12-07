812fb84d10d572d10a04f9c5faf5443e7e344452
Heihokon <neoshark22@gmail.com>
2022-12-07 16:03:38 +0100

    add logInfoSprintf, logDebugSprintf and logInfoSprintf methods in LogTrait

--- src/Traits/LogTrait.php
+++ src/Traits/LogTrait.php
@@ -10,6 +10,48 @@ trait LogTrait
 {
     /**
      * @param FlagshipConfig $config
+     * @param $tag
+     * @param string $message
+     * @param mixed ...$args
+     */
+    protected function logDebugSprintf(FlagshipConfig $config, $tag, $message, $args=[])
+    {
+        if ($config->getLogLevel() < LogLevel::DEBUG || is_null($config->getLogManager())) {
+            return;
+        }
+        $customMessage = sprintf($message, ...$args);
+        $this->logDebug($config, $customMessage, [FlagshipConstant::TAG => $tag]);
+    }
+
+    /**
+     * @param FlagshipConfig $config
+     * @param string $message
+     * @param array $context
+     */
+    protected function logDebug($config, $message, $context = [])
+    {
+        if (!$config || $config->getLogLevel() < LogLevel::DEBUG || is_null($config->getLogManager())) {
+            return;
+        }
+        $flagshipSdk = FlagshipConstant::FLAGSHIP_SDK;
+        $config->getLogManager()->debug("[$flagshipSdk] $message", $context);
+    }
+
+    /**
+     * @param FlagshipConfig $config
+     * @param string $tag
+     * @param string $message
+     * @param mixed ...$args
+     */
+    protected function logErrorSprintf( FlagshipConfig $config, $tag, $message, $args=[]){
+        if ($config->getLogLevel() < LogLevel::ERROR || is_null($config->getLogManager())) {
+            return;
+        }
+        $customMessage = sprintf($message, ...$args);
+        $this->logError($config, $customMessage, [FlagshipConstant::TAG => $tag]);
+    }
+    /**
+     * @param FlagshipConfig $config
      * @param string $message
      * @param array $context
      */
@@ -22,6 +64,13 @@ trait LogTrait
         $config->getLogManager()->error("[$flagshipSdk] $message", $context);
     }
 
+    protected function logInfoSprintf( FlagshipConfig $config, $tag, $message, $args=[]){
+        if ($config->getLogLevel() < LogLevel::INFO || is_null($config->getLogManager())) {
+            return;
+        }
+        $customMessage = sprintf($message, ...$args);
+        $this->logInfo($config, $customMessage, [FlagshipConstant::TAG => $tag]);
+    }
     /**
      * @param FlagshipConfig $config
      * @param string $message
@@ -34,5 +83,25 @@ trait LogTrait
         }
         $flagshipSdk = FlagshipConstant::FLAGSHIP_SDK;
         $config->getLogManager()->info("[$flagshipSdk] $message", $context);
+    }
+
+    protected function getLogFormat($message, $url, $requestBody, $headers, $duration){
+        $format = [];
+        if ($message){
+            $format["message"] = $message;
+        }
+        if ($url){
+            $format["url"] = $url;
+        }
+        if ($requestBody){
+            $format['body'] = $requestBody;
+        }
+        if ($headers){
+            $format['headers'] = $headers;
+        }
+        if ($duration){
+            $format['duration'] = $duration;
+        }
+        return  $format;
     }
 }

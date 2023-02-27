c0df938c9f70c1b9cce56a7efdf2a1aa6be35a57
Heihokon <neoshark22@gmail.com>
2023-02-27 16:17:43 +0100

    add ExposedUserTest.php

---
+++ tests/Model/ExposedUserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Flagship\Model;
+
+use PHPUnit\Framework\TestCase;
+
+class ExposedUserTest extends TestCase
+{
+
+    public function testConstruct()
+    {
+        $visitorId = 'visitorId';
+        $visitorContext = ["key"=>"value"];
+        $anonymousId = "anonymousId";
+        $exposedUser = new ExposedUser($visitorId, $anonymousId, $visitorContext);
+
+        $this->assertSame($visitorId, $exposedUser->getVisitorId());
+        $this->assertSame($anonymousId, $exposedUser->getAnonymousId());
+        $this->assertSame($visitorContext, $exposedUser->getContext());
+    }
+}

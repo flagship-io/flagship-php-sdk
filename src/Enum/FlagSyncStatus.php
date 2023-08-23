0777421cb6b8cac2fd061e5caec06ac60e2bf9b4
Heihokon <neoshark22@gmail.com>
2023-08-23 16:28:30 -0500

    add FlagSyncStatus class

---
+++ src/Enum/FlagSyncStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Flagship\Enum;
+
+class FlagSyncStatus
+{
+    const CREATED = "CREATED";
+    const CONTEXT_UPDATED = "CONTEXT_UPDATED";
+    const FLAGS_FETCHED = "FLAGS_FETCHED";
+    const AUTHENTICATED = "AUTHENTICATED";
+    const UNAUTHENTICATED = "UNAUTHENTICATED";
+}

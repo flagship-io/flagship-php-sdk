9795dea5e65a11fbe3be1f752cea43877160f519
Heihokon <neoshark22@gmail.com>
2023-09-08 16:16:35 -0500

    add getCacheStrategyName method

--- src/Enum/CacheStrategy.php
+++ src/Enum/CacheStrategy.php
@@ -6,4 +6,18 @@ class CacheStrategy
 {
     const BATCHING_AND_CACHING_ON_FAILURE = 1;
     const NO_BATCHING_AND_CACHING_ON_FAILURE = 2;
+
+    private static $listCacheStrategy = ["BATCHING_AND_CACHING_ON_FAILURE", "NO_BATCHING_AND_CACHING_ON_FAILURE"];
+
+    /**
+     * @param int $strategy
+     * @return string
+     */
+    public static function getCacheStrategyName($strategy)
+    {
+        if (!is_int($strategy) || $strategy < 1 || $strategy > 2) {
+            return "";
+        }
+        return self::$listCacheStrategy[$strategy];
+    }
 }

26f36b3fddbc76b25252a69f2c650ecf49d8e152
Heihokon <neoshark22@gmail.com>
2024-01-11 14:53:00 -0600

    Rename to UsageHit

--- src/Hit/UsageHit.php
+++ src/Hit/UsageHit.php
@@ -5,16 +5,10 @@ namespace Flagship\Hit;
 use Flagship\Enum\HitType;
 use Flagship\Hit\Diagnostic;
 
-class Analytic extends Diagnostic
+class UsageHit extends Diagnostic
 {
     public function __construct()
     {
         parent::__construct(HitType::USAGE);
-    }
-
-    public function toApiKeys()
-    {
-        $this->setVisitorId(null);
-        return parent::toApiKeys();
     }
 }
--- tests/Hit/UsageHitTest.php
+++ tests/Hit/UsageHitTest.php
@@ -6,16 +6,15 @@ use Flagship\Config\DecisionApiConfig;
 use PHP_CodeSniffer\Config;
 use PHPUnit\Framework\TestCase;
 
-class AnalyticTest extends TestCase
+class UsageHitTest extends TestCase
 {
     public function testToApiKeys()
     {
         $config = new DecisionApiConfig();
-        $analyticHit = new Analytic();
+        $analyticHit = new UsageHit();
         $analyticHit->setVisitorId("visitor")
         ->setConfig($config);
 
         $this->assertSame('USAGE', $analyticHit->toApiKeys()['t']);
-        $this->assertNull($analyticHit->getVisitorId());
     }
 }

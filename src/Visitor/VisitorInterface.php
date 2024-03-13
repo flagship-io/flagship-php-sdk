8a8b44386e4962197ba774acc5a52939c9a89288
Heihokon <neoshark22@gmail.com>
2024-03-13 14:31:06 -0500

    Remove deprecated methods from VisitorInterface

--- src/Visitor/VisitorInterface.php
+++ src/Visitor/VisitorInterface.php
@@ -43,22 +43,6 @@ interface VisitorInterface extends VisitorCoreInterface
 
 
     /**
-     * Retrieve a modification value by its key. If no modification match the given
-     * key or if the stored value type and default value type do not match, default value will be returned.
-     *
-     * @param string              $key          key associated to the modification.
-     * @param string|bool|numeric|array $defaultValue : default value to return.
-     * @param bool                $activate     Set this parameter to true to automatically
-     *                                          report on our server that the
-     *                                          current visitor has seen this modification. It is possible to call
-     *                                          activateModification() later.
-
-     * @return string|bool|numeric|array : modification value or default value.
-     * @deprecated use getFlag instead
-     */
-    public function getModification($key, $defaultValue, $activate = false);
-
-    /**
      * @param string $key key associated to the flag
      * @param string|bool|numeric|array $defaultValue flag default value.
      * @return FlagInterface
@@ -79,43 +63,8 @@ interface VisitorInterface extends VisitorCoreInterface
     public function unauthenticate();
 
     /**
-     * @deprecated
-     * @return array
-     */
-    public function getModifications();
-
-    /**
      * Return an array of all flags data fetched for the current visitor.
      * @return FlagDTO[]
      */
     public function getFlagsDTO();
-
-    /**
-     * Get the campaign modification information value matching the given key.
-     * @deprecated
-     * @param string $key key which identify the modification.
-     * @return array|null
-     */
-    public function getModificationInfo($key);
-
-    /**
-     * @deprecated
-     * In DecisionApi Mode this function calls the Flagship Decision API to run
-     * campaign assignments according to the current user context
-     * and retrieve applicable modifications. <br/>
-     * In bucketing Mode, it checks bucketing file,
-     * validates campaigns targeting the visitor,
-     * assigns a variation and retrieve applicable modifications
-     * @return void
-     */
-    public function synchronizeModifications();
-
-    /**
-     * Report this user has seen this modification.
-     *
-     * @deprecated
-     * @param string $key key which identify the modification to activate.
-     * @return void
-     */
-    public function activateModification($key);
 }

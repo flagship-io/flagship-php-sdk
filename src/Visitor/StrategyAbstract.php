a1da44f9821b10249f824e76423bdc6b4d0849f6
Heihokon <neoshark22@gmail.com>
2024-03-14 14:12:27 -0500

    Fix analytics tracking and add visitor ID***

--- src/Visitor/StrategyAbstract.php
+++ src/Visitor/StrategyAbstract.php
@@ -70,6 +70,7 @@ abstract class StrategyAbstract implements VisitorCoreInterface, VisitorFlagInte
         $this->visitor = $visitor;
     }
 
+
     /**
      * @return mixed
      */
@@ -130,6 +131,7 @@ abstract class StrategyAbstract implements VisitorCoreInterface, VisitorFlagInte
     {
         return $this->getVisitor()->getConfig();
     }
+
 
 
     /**
@@ -382,7 +384,6 @@ abstract class StrategyAbstract implements VisitorCoreInterface, VisitorFlagInte
         $analytic = new UsageHit();
         $analytic->setLabel(TroubleshootingLabel::SDK_CONFIG)
             ->setLogLevel(LogLevel::INFO)
-            ->setVisitorId($this->getFlagshipInstanceId())
             ->setSdkConfigMode($config->getDecisionMode())
             ->setSdkConfigLogLeve($config->getLogLevel())
             ->setSdkConfigTimeout($config->getTimeout())
@@ -392,9 +393,10 @@ abstract class StrategyAbstract implements VisitorCoreInterface, VisitorFlagInte
             ->setSdkConfigUsingOnVisitorExposed(!!$config->getOnVisitorExposed())
             ->setSdkConfigUsingCustomHitCache(!!$config->getHitCacheImplementation())
             ->setSdkConfigUsingCustomVisitorCache(!!$config->getVisitorCacheImplementation())
-            ->setConfig($config)
             ->setSdkStatus($visitor->getSdkStatus())
-            ->setFlagshipInstanceId($this->getFlagshipInstanceId());
+            ->setFlagshipInstanceId($this->getFlagshipInstanceId())
+            ->setVisitorId($this->getFlagshipInstanceId())
+            ->setConfig($config);
         $this->getTrackingManager()->addUsageHit($analytic);
     }
 
@@ -431,12 +433,9 @@ abstract class StrategyAbstract implements VisitorCoreInterface, VisitorFlagInte
         $troubleshootingHit = new Troubleshooting();
         $troubleshootingHit->setLabel(TroubleshootingLabel::VISITOR_FETCH_CAMPAIGNS)
             ->setLogLevel(LogLevel::INFO)
-            ->setVisitorId($visitor->getVisitorId())
-            ->setAnonymousId($visitor->getAnonymousId())
             ->setVisitorSessionId($visitor->getInstanceId())
             ->setFlagshipInstanceId($visitor->getFlagshipInstanceId())
             ->setTraffic($traffic)
-            ->setConfig($config)
             ->setVisitorAssignmentHistory($assignmentHistory)
             ->setVisitorContext($visitor->getContext())
             ->setSdkStatus($visitor->getSdkStatus())
@@ -454,7 +453,10 @@ abstract class StrategyAbstract implements VisitorCoreInterface, VisitorFlagInte
             ->setSdkConfigUsingOnVisitorExposed(!!$config->getOnVisitorExposed())
             ->setSdkConfigUsingCustomHitCache(!!$config->getHitCacheImplementation())
             ->setSdkConfigUsingCustomVisitorCache(!!$config->getVisitorCacheImplementation())
-            ->setSdkConfigTrackingManagerConfigStrategy($config->getCacheStrategy());
+            ->setSdkConfigTrackingManagerConfigStrategy($config->getCacheStrategy())
+            ->setVisitorId($visitor->getVisitorId())
+            ->setAnonymousId($visitor->getAnonymousId())
+            ->setConfig($config);
 
         $this->sendTroubleshootingHit($troubleshootingHit);
     }

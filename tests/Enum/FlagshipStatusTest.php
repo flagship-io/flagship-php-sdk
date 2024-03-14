d9e7261525c75ed1560743d33175570489b3458f
Heihokon <neoshark22@gmail.com>
2024-03-14 14:14:39 -0500

    Remove redundant test case in FlagshipStatusTest

--- tests/Enum/FlagshipStatusTest.php
+++ tests/Enum/FlagshipStatusTest.php
@@ -9,7 +9,6 @@ class FlagshipStatusTest extends TestCase
     public function testGetStatusName()
     {
         $this->assertEquals("NOT_INITIALIZED", FlagshipStatus::getStatusName(FlagshipStatus::NOT_INITIALIZED));
-        $this->assertEquals("NOT_INITIALIZED", FlagshipStatus::getStatusName(FlagshipStatus::NOT_READY));
         $this->assertEquals("STARTING", FlagshipStatus::getStatusName(FlagshipStatus::STARTING));
         $this->assertEquals("POLLING", FlagshipStatus::getStatusName(FlagshipStatus::POLLING));
         $this->assertEquals("READY_PANIC_ON", FlagshipStatus::getStatusName(FlagshipStatus::READY_PANIC_ON));

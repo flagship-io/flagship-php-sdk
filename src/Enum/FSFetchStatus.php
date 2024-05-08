daadf9bc39c42827d22ff635904c2b7d723b030e
Heihokon <neoshark22@gmail.com>
2024-05-08 15:59:19 -0500

    Add FSFetchStatus enum for flag fetching status

---
+++ src/Enum/FSFetchStatus.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Flagship\Enum;
+
+class FSFetchStatus
+{
+    /**
+     * The flags have been successfully fetched from the API or re-evaluated in bucketing mode.
+     */
+    const FETCHED = 0;
+
+    /**
+     * The flags are currently being fetched from the API or re-evaluated in bucketing mode.
+     */
+    const FETCHING = 1;
+
+    /**
+     * The flags need to be re-fetched due to a change in context, or because the flags were loaded from cache or XPC.
+     */
+    const FETCH_REQUIRED = 2;
+
+    /**
+     * The SDK is in PANIC mode: All features are disabled except for the one to fetch flags.
+     */
+    const PANIC = 3;
+}

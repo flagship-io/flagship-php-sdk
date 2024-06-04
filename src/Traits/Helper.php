018dfe77c787128728824daafb50ac4f9d6472cd
Heihokon <neoshark22@gmail.com>
2024-06-04 14:50:48 -0500

    refactor: Add valueToHex method to Helper trait for converting values to hex

--- src/Traits/Helper.php
+++ src/Traits/Helper.php
@@ -18,4 +18,19 @@ trait Helper
     {
         return new DateTime();
     }
+
+    /**
+     * convert value to hex
+     * @param $value
+     * @return string
+     */
+    public function valueToHex($value)
+    {
+        $jsonString = json_encode($value);
+        $hex = '';
+        for ($i = 0; $i < strlen($jsonString); $i++) {
+            $hex .= dechex(ord($jsonString[$i]));
+        }
+        return $hex;
+    }
 }

c1a31e26ec5a4a0dae5e46b85a28d4c575d7031b
Heihokon <neoshark22@gmail.com>
2024-06-04 10:39:40 -0500

    Refactor FSFlagInterface to include a defaultValue parameter in the getValue() method

--- src/Flag/FSFlagInterface.php
+++ src/Flag/FSFlagInterface.php
@@ -2,18 +2,16 @@
 
 namespace Flagship\Flag;
 
-use Flagship\Model\ExposedFlagInterface;
-
-interface FSFlagInterface extends ExposedFlagInterface
+interface FSFlagInterface
 {
-    /**
-     * Returns the value from the assigned campaign variation or the Flag default value if the Flag does not exist,
-     * or if types are different.
-     *
-     * @param bool $visitorExposed
-     * @return bool|numeric|string|array
-     */
-    public function getValue($visitorExposed = true);
+     /**
+      * Returns the value from the assigned campaign variation or the Flag default value if the Flag does not exist,
+      * or if types are different.
+      * @param bool|numeric|string|array $defaultValue
+      * @param boolean $visitorExposed
+      * @return bool|numeric|string|array
+      */
+    public function getValue($defaultValue, $visitorExposed = true);
 
     /**
      * This method will return true if a Flag exists in Flagship.

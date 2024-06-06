f235dd8ca94687d0ad402caeb1617782e14aa457
Heihokon <neoshark22@gmail.com>
2024-06-06 12:10:08 -0500

    refactor: Update FSSdkStatus enum to use PHP 8.1 enum syntax

--- src/Enum/FSSdkStatus.php
+++ src/Enum/FSSdkStatus.php
@@ -2,25 +2,28 @@
 
 namespace Flagship\Enum;
 
-class FSSdkStatus extends EnumStatusBase
+/**
+ * Enum FSSdkStatus
+ */
+enum FSSdkStatus: int
 {
     /**
      * It is the default initial status. This status remains until the sdk has been initialized successfully.
      */
-    const SDK_NOT_INITIALIZED = 0;
+    case SDK_NOT_INITIALIZED = 0;
 
     /**
      * The SDK is currently initializing.
      */
-    const SDK_INITIALIZING = 1;
+    case SDK_INITIALIZING = 1;
 
     /**
      * Flagship SDK is ready but is running in Panic mode: All features are disabled except the one which refresh this status.
      */
-    const SDK_PANIC = 2;
+    case SDK_PANIC = 2;
 
     /**
      * The Initialization is done, and Flagship SDK is ready to use.
      */
-    const SDK_INITIALIZED = 3;
+    case SDK_INITIALIZED = 3;
 }

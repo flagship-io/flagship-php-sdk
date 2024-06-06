8b91ba31895e1a9bc49f41373d47907c58789bce
Heihokon <neoshark22@gmail.com>
2024-06-06 13:08:54 -0500

    refactor: Update FSFlagStatus enum to use PHP 8.1 enum syntax

--- src/Enum/FSFlagStatus.php
+++ src/Enum/FSFlagStatus.php
@@ -2,25 +2,28 @@
 
 namespace Flagship\Enum;
 
-class FSFlagStatus extends EnumStatusBase
+/**
+ * FSFlagStatus Enum
+ */
+enum FSFlagStatus: int
 {
     /**
      * The flags have been successfully fetched from the API or re-evaluated in bucketing mode.
      */
-    const FETCHED = 0;
+    case FETCHED = 0;
 
     /**
      * The flags need to be re-fetched due to a change in context, or because the flags were loaded from cache or XPC.
      */
-    const FETCH_REQUIRED = 1;
+    case FETCH_REQUIRED = 1;
 
     /**
      * The flag was not found or do not exist.
      */
-    const NOT_FOUND = 2;
+    case NOT_FOUND = 2;
 
     /**
      * The SDK is in PANIC mode: All features are disabled except for the one to fetch flags.
      */
-    const PANIC = 3;
+    case PANIC = 3;
 }

35d00693e4543063575de0be1b7ae6f88bb71fbc
Heihokon <neoshark22@gmail.com>
2024-06-06 12:10:04 -0500

    refactor: Update LogLevel enum to use PHP 8.1 enum syntax

--- src/Enum/LogLevel.php
+++ src/Enum/LogLevel.php
@@ -2,59 +2,55 @@
 
 namespace Flagship\Enum;
 
-class LogLevel 
+enum LogLevel: int
 {
     /**
-     * NONE = 0: Logging will be disabled.
+     * Logging will be disabled.
      */
-    const NONE      = 0;
-    /**
-     * EMERGENCY = 1: Only emergencies will be logged.
-     */
-    const EMERGENCY = 1;
-    /**
-     * ALERT = 2: Only alerts and above will be logged.
-     */
-    const ALERT = 2;
-    /**
-     * CRITICAL = 3: Only critical and above will be logged.
-     */
-    const CRITICAL = 3;
-    /**
-     * ERROR = 4: Only errors and above will be logged.
-     */
-    const ERROR = 4;
-    /**
-     * WARNING = 5: Only warnings and above will be logged.
-     */
-    const WARNING = 5;
-    /**
-     * NOTICE = 6: Only notices and above will be logged.
-     */
-    const NOTICE  = 6;
-    /**
-     * INFO = 7: Only info logs and above will be logged.
-     */
-    const INFO = 7;
-    /**
-     * DEBUG = 8: Only debug logs and above will be logged.
-     */
-    const DEBUG = 8;
+    case NONE = 0;
 
     /**
-     * ALL = 9: All logs will be logged.
+     * Only emergencies will be logged.
      */
-    const ALL = 9;
+    case EMERGENCY = 1;
 
     /**
-     * @param int $loglevel
-     * @return string
+     * Only alerts and above will be logged.
      */
-    public static function getLogName($value)
-    {
-        $class = new \ReflectionClass(__CLASS__);
-        $constants = array_flip($class->getConstants());
-    
-        return isset($constants[$value]) ? $constants[$value] : "";
-    }
+    case ALERT = 2;
+
+    /**
+     * Only critical and above will be logged.
+     */
+    case CRITICAL = 3;
+
+    /**
+     * Only errors and above will be logged.
+     */
+    case ERROR = 4;
+
+    /**
+     * Only warnings and above will be logged.
+     */
+    case WARNING = 5;
+
+    /**
+     * Only notices and above will be logged.
+     */
+    case NOTICE = 6;
+
+    /**
+     * Only info logs and above will be logged.
+     */
+    case INFO = 7;
+
+    /**
+     * Only debug logs and above will be logged.
+     */
+    case DEBUG = 8;
+
+    /**
+     * All logs will be logged.
+     */
+    case ALL = 9;
 }

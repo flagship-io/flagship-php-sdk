cdc034655ec944bdc9ac583cbe76ae99e6f97892
Heihokon <neoshark22@gmail.com>
2024-06-17 16:20:20 -0500

    refactor: Update cache interfaces to use PHP 8.1 typed properties and return types

--- src/Cache/IHitCacheImplementation.php
+++ src/Cache/IHitCacheImplementation.php
@@ -5,28 +5,30 @@ namespace Flagship\Cache;
 interface IHitCacheImplementation
 {
     /**
-     * This method will be called to cache visitor hits when a hit has failed to be sent if there is no internet, there has been a timeout or if the request responded with something > 2XX.
+     * This method will be called to cache visitor hits when a hit has failed to be sent if there is no internet,
+     * there has been a timeout or if the request responded with something > 2XX.
      * @param array $hits
      * @return void
      */
-    public function cacheHit(array $hits);
+    public function cacheHit(array $hits): void;
 
     /**
-     * This method will be called to load hits corresponding to visitor ID from your database and trying to send them again in the background.
+     * This method will be called to load hits corresponding to visitor ID
+     * from your database and trying to send them again in the background.
      * Note: Hits older than 4H will be ignored
      * @return array
      */
-    public function lookupHits();
+    public function lookupHits(): array;
 
     /**
      * This method will be called to erase the visitor hits cache corresponding to visitor ID from your database.
      * @param array $hitKeys
      * @return void
      */
-    public function flushHits(array $hitKeys);
+    public function flushHits(array $hitKeys): void;
 
     /**
      * @return void
      */
-    public function flushAllHits();
+    public function flushAllHits(): void;
 }
--- src/Cache/IVisitorCacheImplementation.php
+++ src/Cache/IVisitorCacheImplementation.php
@@ -10,19 +10,21 @@ interface IVisitorCacheImplementation
      * @param array $data
      * @return void
      */
-    public function cacheVisitor($visitorId, array $data);
+    public function cacheVisitor(string $visitorId, array $data): void;
 
     /**
-     * This method is called when the SDK needs to get the visitor information corresponding to visitor ID from your database.
+     * This method is called when the SDK needs to get the visitor
+     * information corresponding to visitor ID from your database.
      * @param string $visitorId
      * @return array
      */
-    public function lookupVisitor($visitorId);
+    public function lookupVisitor(string $visitorId): array;
 
     /**
-     * This method is called when the SDK needs to erase the visitor information corresponding to visitor ID in your database.
+     * This method is called when the SDK needs to erase the visitor
+     * information corresponding to visitor ID in your database.
      * @param string $visitorId
      * @return void
      */
-    public function flushVisitor($visitorId);
+    public function flushVisitor(string $visitorId): void;
 }

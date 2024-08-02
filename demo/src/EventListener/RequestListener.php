e5c9a3257f7743d25cbc43431bb2fbcb6f1e4051
Heihokon <neoshark22@gmail.com>
2024-08-02 13:05:40 -0500

    feat: Add RequestListener class for handling kernel requests

---
+++ demo/src/EventListener/RequestListener.php
@@ -0,0 +1,16 @@
+<?php
+
+// src/EventListener/RequestListener.php
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class RequestListener
+{
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+    }
+}
